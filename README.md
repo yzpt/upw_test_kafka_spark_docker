ca24685864c021d4e8e1aa2ecbdc0eadb0968593
Yohann <yzpt.de@gmail.com>
2023-12-17 20:35:21 +0100

    authorization ongoing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-[journal.sh](journal.sh)
+# Kafka Authorization by Example
+
+Well, well, well, let's have ourselves a Kafka ACL example party. ;) 
+
+[Kafka Authorization Tutorial](https://supergloo.com/kafka-tutorials/kafka-acl-authorization/)
+
+The Kafka ACL tutorial linked above uses the config files used in this directory.
+
+I know, you are completely shocked right?
+
--- journal.sh
+++ journal.sh
@@ -12,10 +12,32 @@ kafka-topics.sh --create --topic test-topic-auth  --bootstrap-server localhost:9
 kafka-topics.sh --list test-topic-auth  --bootstrap-server localhost:9092 --command-config etc/kafka/client.properties
 # ok !
 
-git add . && git commit -m "authentification ok"
+git add . && git commit -m "authorization ongoing"
 git push --set-upstream origin zookeeper_acl
 
 
 # 2. === Authorization ================================================================================
 https://supergloo.com/kafka-tutorials/kafka-acl/
+
+cp kafka-examples/authorization/* .
+
+
+docker compose -f kafka-authorization-example.yml up -d
+
+docker exec -it upw_test_docker_kafka_spark_nosql-kafka-1 /bin/bash
+kafka-topics.sh --create --topic test-topic-auth --bootstrap-server localhost:9092 --command-config /etc/kafka/alice-client.properties
+# error : JMX 1099 binded, comment the 'JMX_PORT: 1099' line in docker-compose.yml then ok
+kafka-topics.sh --list --bootstrap-server localhost:9092 --command-config /etc/kafka/alice-client.properties
+kafka-topics.sh --list --bootstrap-server localhost:9092 # to check if authentification is working (waiting timeout)
+
+# Let’s have Madhu and Alice both produce some gibberish to the test-topic-auth topic to show they can both publish to it.
+
+kafka-console-producer.sh --topic test-topic-auth --broker-list localhost:9092 --producer.config /etc/kafka/alice-client.properties
+kafka-console-producer.sh --topic test-topic-auth --broker-list localhost:9092 --producer.config /etc/kafka/madhu-client.properties
+
+# And now, prove they can both consume from it.
+kafka-console-consumer.sh --topic test-topic-auth --bootstrap-server localhost:9092 --consumer.config /etc/kafka/alice-client.properties --from-beginning
+kafka-console-consumer.sh --topic test-topic-auth --bootstrap-server localhost:9092 --consumer.config /etc/kafka/madhu-client.properties --from-beginning
+
+
 
