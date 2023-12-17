fb37212c334b34c9ae1437225a4c60ac57b7ae45
Yohann <yzpt.de@gmail.com>
2023-12-17 16:07:13 +0100

    -

--- journal.sh
+++ journal.sh
@@ -31,14 +31,11 @@ python3 python2_consumer.py
 
 
 # === Spark ========================================================================
-
-
 docker compose down  spark-master spark-worker
 docker compose up -d spark-master spark-worker  
+
 # start streaming:
 docker exec -it spark-master /bin/bash -c "spark-submit --master local --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,com.datastax.spark:spark-cassandra-connector_2.12:3.4.1 /opt/bitnami/pyspark_scripts/spark_streaming.py"
-
-# ok
 
 
 # === Cassandra ========================================================================
@@ -85,15 +82,7 @@ truncate spark_streaming.messages;
 
 
 
-# === Kafka python ACL ========================================================================
-docker compose up -d kafka
-
-docker cp kafka:/opt/bitnami/kafka/config/server.properties .
-# -> server.properties
-
-
-
-# === git
+# === git =========================================================
 git init
 touch .gitignore && code .gitignore
 git add .
@@ -104,3 +93,112 @@ git push --set-upstream origin main
 touch README.MD && code README.MD
 
 git add . && git commit -m "update" && git push
+
+
+# === Zookeeper mode ========================================================================
+# no ACL with Kraft mode !
+
+git checkout -b zookeeper_mode
+
+# -> docker-compose
+docker compose up -d zookeeper 
+docker compose up -d zookeeper broker 
+docker compose up -d zookeeper broker schema-registry 
+docker compose up -d zookeeper broker schema-registry control-center
+
+# docker create topic
+docker exec broker kafka-topics --create --topic test-topic --bootstrap-server broker:9092
+
+# python producer
+python3 python1_producer.py "allo"
+
+# deploy pipeline
+docker compose up -d zookeeper broker schema-registry control-center spark-master spark-worker cassandra
+
+# start spark streaming
+docker exec -it spark-master /bin/bash -c "spark-submit --master local --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,com.datastax.spark:spark-cassandra-connector_2.12:3.4.1 /opt/bitnami/pyspark_scripts/spark_streaming.py"
+
+# send a message
+python3 python1_producer.py "message without h_elp word"
+python3 python1_producer.py "message with help word"
+
+# check cassandra
+docker exec -it cassandra /bin/bash
+cqlsh -u cassandra -p cassandra
+select value, destination_table from spark_streaming.help;
+select value, destination_table from spark_streaming.messages;
+
+# no insertion in cassandra
+# -> spark_streaming.py change "kafka:9092" to "broker:29092" and restart spark streaming
+
+docker compose down spark-master spark-worker
+docker compose up -d spark-master spark-worker
+docker exec -it spark-master /bin/bash -c "spark-submit --master local --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,com.datastax.spark:spark-cassandra-connector_2.12:3.4.1 /opt/bitnami/pyspark_scripts/spark_streaming.py"
+
+python3 python1_producer.py "without the word"
+python3 python1_producer.py "with help word"
+
+# check cassandra --> pipeline ok.
+git add . && git commit -m "zookeeper mode pipeline ok"
+git push --set-upstream origin zookeeper_mode
+
+# === ACL ==============================================================================
+# https://www.youtube.com/watch?v=bj5SKXanaAI
+
+docker compose down
+docker compose up -d zookeeper broker
+
+mkdir configs
+touch configs/kafka_server_jaas.conf && code configs/kafka_server_jaas.conf
+# --- kafka_server_jaas.conf ---
+KafkaServer {
+    org.apache.kafka.common.security.plain.PlainLoginModule required
+    username="admin"
+    password="pass123"
+    user_kafka="pass123";
+}
+# ------------------------------
+
+touch configs/config.properties && code configs/config.properties
+# --- config.properties ---
+sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username="admin" password="pass123";
+security.protocol=SASL_PLAINTEXT
+sasl.mechanism=PLAIN
+# ------------------------------
+
+docker compose down
+docker compose up -d zookeeper broker
+
+docker exec -it broker /bin/bash
+kafka-topics --create --topic allo-topic --bootstrap-server broker:9093 --replication-factor 1 --partitions 1 --command-config /etc/kafka/configs/config.properties
+
+# === stop
+git add . && git commit -m "stop, go SASL branch"
+git push --set-upstream origin acl
+
+git checkout -b sasl
+
+# === SASL ==============================================================================
+https://jaehyeon.me/blog/2023-07-13-kafka-development-with-docker-part-10/
+
+git add . && git commit -m "-"
+git push --set-upstream origin sasl
+
+git clone https://github.com/jaehyeon-kim/kafka-pocs.git
+
+cp kafka-pocs/kafka-dev-with-docker/part-10/generate.sh .
+cp kafka-pocs/kafka-dev-with-docker/part-10/kafka-hosts.txt .
+cp kafka-pocs/kafka-dev-with-docker/part-10/command.properties .
+cp kafka-pocs/kafka-dev-with-docker/part-10/client.properties .
+source generate.sh
+
+docker compose down
+docker compose up -d
+
+
+git checkout main
+
+# === Back to KRaft mode with ACL ========================================================
+git add . && git commit -m "-"
+git push --set-upstream origin main
+
